8cd3ecd72ce7ea7ab039e9c70bc2daaa23b0954d
Diego Carravetta <diegocarravetta@icloud.com>
2021-10-19 00:09:45 -0300

    Aula 16 - Parte1

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express = require (`express`);
 const path = require (`path`);
 
+require (`dotenv`).config ()
+
 const app = express ();
-const port = process.env.Port || 3000;
+const port = process.env.PORT;
+
+app.use (express.urlencoded({ extended: true }));
 
 app.set (`view engine`, `ejs`);
 app.use (express.static (path.join (__dirname, `public`)));
 
+const carros = [
+    {
+        "id": 1,
+        "marca": "Audi",
+        "nome": "R8",
+        "modelo": "Sport",
+        "ano": 2021,
+        "valor": 750,
+        "imagem": "/img/car-card.png"
+    }
+]
+
 app.get (`/`, (req, res) => {
-    res.render (`index`);
+    res.render (`index`, {
+        carros,
+    });
 });
 
 app.get (`/catalogar`, (req, res) => {
